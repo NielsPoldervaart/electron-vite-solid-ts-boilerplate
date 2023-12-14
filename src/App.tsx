9b2c658d2d1740dba539c3ebdc3e6460add8f7d8
NielsPoldervaart <Niels@meghira.nl>
2023-12-14 15:52:24 +0100

    Added footer and update checks.

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,36 @@ import { createSignal } from "solid-js";
 
 import { Counter } from "./components/Counter";
 import Titlebar from "./components/Titlebar";
+import Footer from "./components/Footer";
 
 export const App = () => {
 	const [isMaximized, setIsMaximized] = createSignal(false);
+	const [updateAvailable, setUpdateAvailable] = createSignal(false);
 
 	const handleWindowMaximizedChange = (maximized: boolean) => {
 		setIsMaximized(maximized);
 	};
 
+	const handleUpdateAvailable = (available: boolean) => {
+		setUpdateAvailable(available);
+	};
+
 	// When window is maximized / unmaximized call callback function.
 	window.api.onWindowMaximizedChange(handleWindowMaximizedChange);
+
+	// When window is maximized / unmaximized call callback function.
+	window.api.onUpdateAvailable(handleUpdateAvailable);
 
 	return (
 		<>
 			<Titlebar isMaximized={isMaximized()} />
 			<div class="main-content">
 				<h1>SolidJS + Vite + TypeScript</h1>
-				<p>v{APP_VERSION}</p>
 				<div class="card">
 					<Counter />
 				</div>
 			</div>
+			<Footer updateAvailable={updateAvailable()} />
 		</>
 	);
 };
