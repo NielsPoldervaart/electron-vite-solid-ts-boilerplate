2cec851a8833ac7bbb1bc172b4e7e91600297812
NielsPoldervaart <Niels@meghira.nl>
2024-02-22 23:39:10 +0100

    Remove unused code and update Titlebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { createSignal } from "solid-js";
-
 import Counter from "./components/Counter/Counter";
 import Titlebar from "./components/Titlebar/Titlebar";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 const App = () => {
-	const [isMaximized, setIsMaximized] = createSignal(false);
-
-	const handleWindowMaximizedChange = (maximized: boolean) => {
-		setIsMaximized(maximized);
-	};
-
-	// When window is maximized / unmaximized call callback function.
-	window.api.onWindowMaximizedChange(handleWindowMaximizedChange);
-
 	return (
 		<>
-			<Titlebar isMaximized={isMaximized()} />
+			<Titlebar />
 			<div class="mainContent">
 				<h1>SolidJS + Vite + TypeScript</h1>
 				<div class="card">
